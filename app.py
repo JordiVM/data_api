d42e80609b695bad482747ae5683271f1a5018db
Jordi Vives <jordivives94@gmail.com>
2022-01-24 21:42:30 +0100

    Add app skeleton, includes app factory and db init with basic configuration

---
+++ app.py
@@ -0,0 +1,4 @@
+from data_api import create_app
+
+# Call application factory to start a Flask instance
+app = create_app("flask.cfg")
---
+++ data_api/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Create instance for flask SQL extension, not yet attached to application
+db = SQLAlchemy()
+
+
+# Flask application factory
+
+def create_app(config_filename=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_filename)
+    initialize_extensions(app)
+    return app
+
+
+def initialize_extensions(app):
+    # Bind extensions to the Flask application instance (app)
+    db.init_app(app)
