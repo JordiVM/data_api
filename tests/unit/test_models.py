2e1a123c23525c66573a45e5fa9ce1bcc25e393d
Jordi Vives <jordivives94@gmail.com>
2022-01-24 22:30:50 +0100

    Test for Conversation class constructor

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,13 +1,25 @@
-from data_api.models import Message
+from data_api.models import Message, Conversation
 
 
 def test_new_message():
     """
     GIVEN a MESSAGE model
     WHEN a new MESSAGE is created
-    THEN check the language and date are defined correctly
+    THEN check language, text and date are defined correctly
     """
     message = Message("EN", "A short string that will be a message")
     assert message.language == "EN"
     assert message.text == "A short string that will be a message"
     assert message.timestamp is not None
+
+
+def test_new_Conversation():
+    """
+    GIVEN a Conversation model
+    WHEN a new CONVERSATION is created
+    THEN check customer_id, dialog_id and messages are defined correctly
+    """
+    conversation = Conversation(2, 10)
+    assert conversation.customer_id == 2
+    assert conversation.dialog_id == 10
+    assert conversation.messages == []
