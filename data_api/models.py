86504bc45e3334f709e81beb1c5dec5c82d5e13d
Jordi Vives <jordivives94@gmail.com>
2022-01-24 22:30:50 +0100

    Document Messages and Conversation classes and constructor

--- data_api/models.py
+++ data_api/models.py
@@ -2,14 +2,38 @@ from datetime import datetime
 
 
 class Message:
-    def __init__(self, language, text):
+    """
+    Class to represent a message in the chat
+
+    * timestamp : float - indicates date and time of message creation
+    * language : str - language to classify the text in the message
+    * text : str - contents of the message
+    """
+
+    def __init__(self, language: str, text: str):
+        """
+        Create a new Message object using language and text
+        """
         self.timestamp = datetime.timestamp(datetime.now())
         self.language = language
         self.text = text
 
 
 class Conversation:
-    def __init__(self, customer_id, dialog_id):
+    """
+    Class to represent a conversation from the chat
+
+    * customer_id : int - unique ID that identifies the customer that
+      engaged in the chat
+    * dialog_id : str - ID that identifies the dialog
+    * messages : list[Message] - contents of the message
+    """
+
+    def __init__(self, customer_id: int, dialog_id: int):
+        """
+        Create a new Conversation object using customer_id and dialog_id.
+        messages will always start empty
+        """
         self.customer_id = customer_id
         self.dialog_id = dialog_id
         self.messages = []
