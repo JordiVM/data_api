b6bcae2d74dcfe00f89046ec286442928e6d0b39
Jordi Vives <jordivives94@gmail.com>
2022-01-24 22:30:50 +0100

    Define Conversation class, contains all information to be stored from chat

--- data_api/models.py
+++ data_api/models.py
@@ -6,3 +6,10 @@ class Message:
         self.timestamp = datetime.timestamp(datetime.now())
         self.language = language
         self.text = text
+
+
+class Conversation:
+    def __init__(self, customer_id, dialog_id):
+        self.customer_id = customer_id
+        self.dialog_id = dialog_id
+        self.messages = []
