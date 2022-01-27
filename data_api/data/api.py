6811abde9b34faf19ef15e63d44b9d08eaf81231
Jordi Vives <jordivives94@gmail.com>
2022-01-27 21:14:37 +0100

    Add get /data/?language=language|customerID=customerID support with db query

--- data_api/data/api.py
+++ data_api/data/api.py
@@ -25,8 +25,9 @@ def post_message(customer_id: int, dialog_id: int):
 
 @data_bp.route("/consents/<int:dialog_id>", methods=["POST"])
 def post_consent(dialog_id: int):
-    consent = request.form["consent"]  # assumes consent is str
+    consent = request.form["consent"]
 
+    # assumes consent is str
     if consent == "true":
         for message in conversationPool[dialog_id].messages:
             entry = MessageEntry(
@@ -46,15 +47,32 @@ def post_consent(dialog_id: int):
 
 @data_bp.route("/data/", methods=["GET"])
 def get_messages():
-    language = request.args.get("language")
-    customer_id = request.args.get("customerId")
+    # retrieves language, customerId, limit, and offset from query
+    language = request.args.get("language", type=str)
+    customer_id = request.args.get("customerId", type=int)
+    # limit and offset are used for pagination
+    # client will be able to control the amount of records to retrieve
+    limit = request.args.get("limit", 100, type=int)
+    offset = request.args.get("offset", 0, type=int)
+
+    if language is None and customer_id is None:
+        return "", 400
 
     result = (
         db.session.query(MessageEntry)
-        .filter_by(language=language, customer_id=customer_id)
-        .all()
+        .order_by(MessageEntry.date.desc())
+        .filter(
+            (MessageEntry.language == language)
+            | (MessageEntry.customer_id == customer_id)
+        )
+        .limit(limit)
+        .offset(offset)
     )
+    dict_result = [r.serialize() for r in result]
 
-    print(result)
+    # Return "" instead of "[]"
+    if len(dict_result) == 0:
+        return "", 200
 
+    # Returns query results in list[json]
     return "", 200
