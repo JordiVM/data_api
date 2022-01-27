13a028e5845a42febe7931ca5d2f193b4694ea27
Jordi Vives <jordivives94@gmail.com>
2022-01-27 21:06:51 +0100

    Add test to serialize() and generalize Message parameters

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,7 @@
-from data_api.models import Message, Conversation
+from datetime import datetime
+
+from data_api.models import Message, Conversation, MessageEntry
+from data_api.utils import random_string
 
 
 def test_new_message():
@@ -31,8 +34,39 @@ def test_add_message():
     WHEN a new message is added
     THEN check message is added at the end of the list
     """
-    conversation = Conversation(2, 10)
-    message = Message("EN", "Some text to verify")
+    conversation = Conversation(3, 10)
+    random_text = random_string(50)
+    random_language = random_string(10)
+    message = Message(random_language, random_text)
     conversation.add_message(message)
-    assert conversation.messages[-1].text == "Some text to verify"
-    assert conversation.messages[-1].language == "EN"
+    assert conversation.messages[-1].text == random_text
+    assert conversation.messages[-1].language == random_language
+
+
+def test_serialize_entry():
+    """
+    GIVEN a MessageEntry instance
+    WHEN entry has to be serialized
+    THEN check columns from MessageEntry are added to the dict
+    """
+    date = datetime.now()
+    customer_id = 1
+    dialog_id = 0
+    text = "Text to serialize"
+    language = "EN"
+
+    entry = MessageEntry(
+        date=date,
+        customer_id=customer_id,
+        dialog_id=dialog_id,
+        text=text,
+        language=language,
+    )
+
+    serialized_entry = entry.serialize()
+
+    assert serialized_entry["date"] == str(date)
+    assert serialized_entry["customer_id"] == customer_id
+    assert serialized_entry["dialog_id"] == dialog_id
+    assert serialized_entry["text"] == text
+    assert serialized_entry["language"] == language
